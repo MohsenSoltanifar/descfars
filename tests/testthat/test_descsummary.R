e5a0ee8d4b1c8fab9549beb047ca48e9458b7d77
MohsenSoltanifar <66443526+MohsenSoltanifar@users.noreply.github.com>
2022-07-19 00:54:15 -0700

    Add files via upload

--- tests/testthat/test_descsummary.R
+++ tests/testthat/test_descsummary.R
@@ -3,9 +3,9 @@ library(descfars)
 devtools::load_all()
 
 
-# test_that("Check number of fatalities in July 2015", {
-#   testthat::expect_equal(nrow((fars_summarize_years(2015))[7,2]), 2998)
-# })
+test_that("Check number of fatalities in July 2015", {
+testthat::expect_equal(as.numeric(fars_summarize_years(2015)[7,2]), 2998)
+})
 
 
 # test_that("Check number of rows for 2013 accidents file", {
@@ -17,8 +17,8 @@ devtools::load_all()
 
 
 #this test loads in the 2013 fars data file, and checks the length of the STATE column
-data = fars_read(make_filename(2013))
-expect_that(length(data$STATE),equals(30202))
+#data = fars_read(make_filename(2013))
+#expect_that(length(data$STATE),equals(30202))
 
 
 
